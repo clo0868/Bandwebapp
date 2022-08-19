5422f6ab6b1b8a09c835760e7911e155105bae42
clo0868 <60488913+clo0868@users.noreply.github.com>
2022-08-19 13:32:31 +1200

    finding some errors

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
     }).then(res => {
       setUser(res.data)
     }).catch(e => {
+      console.log(e);
       if(e.response.data.error && e.response.data.error === 'User not approved!'){
         setUser({user:e.response.data.error})
       }else{
